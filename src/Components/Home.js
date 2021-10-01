95c1f6837b85eff5ec09642ec1a13adebe8a549a
Chigbogu Orji <brightorji60@gmail.com>
2021-10-01 20:21:19 +0100

    Style: Redesign landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { Facebook, Twitter, GitHub } from '@mui/icons-material';
-import Link from '@mui/material/Link';
 import { Link as RLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 import bg from '../assets/kk.png';
@@ -15,95 +14,87 @@ const Home = () => {
       maxWidth="xl"
       disableGutters
       sx={{
-        background: 'white'
+        background: '#fff',
+        display: 'flex',
+        minHeight: '100vh',
+        flex: 1
       }}>
       <Box
-        component="section"
         sx={{
-          background: 'url(' + bg + ')',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: '140% 60%',
-          backgroundSize: '1000px',
-          // backgroundBlendMode: 'light',
-          position: 'relative',
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          width: '100vw',
-          height: '100vh',
+          alignItems: 'flex-end',
+          minHeight: '100vh',
+          width: '40%',
         }}>
-        <Box sx={{width: {lg: 700}}}>
+        <Box sx={{
+          width: { lg: 700 },
+          height: '60%',
+          pl: { xs: 2, md: 10 },
+        }}>
         <Typography
           variant="h1"
-          component="h1"
+            component="h1"
+            color="primary.light"
           sx={{
-            fontSize: {xs: 40, md: 90},
-            fontWeight: 'bold',
-            textAlign: 'right'
+            fontSize: {xs: 40, md: 60},
+            fontWeight: 'medium',
+            mb: 3
           }}>
-            Kode<br/>Konsultant
+            Konsultant
         </Typography>
-        <Box sx={{textAlign: 'right', color: 'grey'}}>
-          <Typography component="p">Your Code</Typography>
-          <Typography component="p">Personal Assistance</Typography>
+          <Box sx={{
+            color: 'grey',
+            fontWeight: 'medium',
+          }}>
+            <Typography
+              variant="h4"
+              component="h5"
+              color="text.secondary"
+            >Your Code Personal Assistance</Typography>
+            <Typography
+              variant="body2"
+              component="p"
+              mt={1}
+              color="grey.600"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui vel obcaecati atque exercitationem distinctio aliquid natus dolorum eum, mollitia adipisci iste inventore perspiciatis maxime quaerat molestias recusandae magnam eos.</Typography>
+          <Button
+            component={RLink}
+            to="/login"
+            sx={{
+              borderRadius: '50px',
+              mt: 5,
+              color: 'warning.contrastText',
+              bgcolor: 'warning.light',
+              textDecoration: 'none',
+              textTransform: 'none',
+              p: 2,
+              ':hover': {bgcolor: 'primary.light', color: 'light'}
+            }}>Schedule a meeting</Button>
         </Box>
         </Box>
-        <Typography
-          component={RLink}
-          to="/login"
-          sx={{
-            borderRadius: '5px',
-            position: 'absolute',
-            bottom: 60,
-            border: '1px solid #1976d2',
-            color: '#fff',
-            bgcolor: 'primary.light',
-            textDecoration: 'none',
-            alignSelf: 'center',
-            p: 2,
-            ':hover': {bgcolor: 'primary.dark', color: 'light'}
-          }}>Schedule An Appointment</Typography>
-
+      </Box>
+      
+      <Box
+        sx={{
+        bgcolor: 'primary.light',
+        display: 'flex',
+        alignItems: 'center',
+        minHeight: '100vh',
+        width: '60%',
+        borderBottomLeftRadius: '100%'
+        }}>
         <Box
           sx={{
-            position: 'fixed',
-            right: 0,
-            top: 0,
-            bottom: 0,
-            width: 60,
-            bgcolor: 'primary.dark',
-            display: 'flex',
-            flexDirection: 'column',
-          }}>
-          <Box sx={{
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            pb: 4
-          }}>
-            <Typography
-              component={Link}
-              sx={{color: '#fff'}}
-              href="https://github.com/chiorji/konsultant" target="_blank" rel="noopener noreferrer">
-              <GitHub fontSize="large"/>
-            </Typography>
-            <Typography
-              component={Link}
-              sx={{my: 2, color: '#fff'}}
-              href="https://www.facebook.com/orjichigbogu" target="_blank" rel="noopener noreferrer">
-              <Facebook fontSize="large"/>
-            </Typography>
-            <Typography
-              component={Link}
-              sx={{color: '#fff'}}
-              href="https://www.twitter.com/@ChigboguOrji" target="_blank" rel="noopener noreferrer">
-              <Twitter fontSize="large"/>
-            </Typography>
-          </Box>
-        </Box>
+            bgcolor: 'primary.light',
+            height: '400px',
+            width: '400px',
+            borderRadius: '50%',
+            background: 'url(' + bg + ')',
+            backgroundRepeat: 'no-repeat',
+            border: '4px solid #42a5f5',
+            backgroundSize: 'cover',
+          }}
+        ></Box>
       </Box>
     </Container>
   )
