33302e21733e6c44b1d7b0b60dbb767fe735f961
Chigbogu Orji <brightorji60@gmail.com>
2021-10-01 09:25:35 +0100

    Update link labels

--- src/Components/ClientAuthTopNav.js
+++ src/Components/ClientAuthTopNav.js
@@ -4,21 +4,47 @@ import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import { Link as RLink } from 'react-router-dom';
 
 import * as action from '../store/auth/actions';
 
-const ClientAuthTopNav = ({logout}) => {
+const ClientAuthTopNav = ({logout, fullname}) => {
   return (
     <AppBar position="fixed" elevation={0} sx={{bgcolor: 'primary.dark'}}>
-      <Container sx={{display: 'flex', py: 2}}>
+      <Container sx={{ display: 'flex', py: 2 }}>
+        <Box sx={{
+          flexGrow: 1,
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}>
+          <Typography
+          variant="h6"
+          component="h5"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontWeight: 'bold',
+            height: '40px',
+            width: '40px',
+            borderRadius: '50%',
+            bgcolor: '#fff',
+            color: 'primary.dark',
+            mr: 2
+          }}>
+        {fullname.slice(0, 2).toUpperCase()}
+        </Typography>
         <Typography
           variant="h5"
           component={RLink}
           to="/"
-          sx={{ flexGrow: 1, fontWeight: 'medium', textDecoration: 'none', color: '#fff' }}>
-          Konsultant
+          sx={{ fontWeight: 'medium', textDecoration: 'none', color: '#fff' }}>
+          {fullname}
         </Typography>
+        </Box>
         <Typography sx={{
           minWidth: '40%',
           display: 'flex',
@@ -29,12 +55,12 @@ const ClientAuthTopNav = ({logout}) => {
             component={RLink}
             to='/dashboard/ap/book'
             sx={{mr: 3, color: '#fff', textDecoration: 'none'}}
-          >Book Appointment</Typography>
+          >New Meeting</Typography>
           <Typography
           component={RLink}
             to='/dashboard/ap/all'
             sx={{mr: 3, color: '#fff', textDecoration: 'none'}}
-          >List Appointments</Typography>
+          >My Calendar</Typography>
           <Button
             size="medium"
             variant="contained"
@@ -52,4 +78,8 @@ const mapDispatch = (dispatch) => ({
   logout: () => dispatch(action.logout())
 })
 
-export default connect(null, mapDispatch)(ClientAuthTopNav);
+const mapState = ({auth}) => ({
+  fullname: auth.fullname
+})
+
+export default connect(mapState, mapDispatch)(ClientAuthTopNav);
