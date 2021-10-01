951f5794de82dc42fa9b2888c7b91ed36dd75e2c
Chigbogu Orji <brightorji60@gmail.com>
2021-10-01 20:39:02 +0100

    Style: Modify top navigation, add social icons

--- src/Components/UnAuthTopNav.js
+++ src/Components/UnAuthTopNav.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import MenuIcon from '@mui/icons-material/Menu';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { Twitter, GitHub } from '@mui/icons-material';
 import { Link as RLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
 
-const UnAuthTopNav = () => {
+const UnAuthTopNav = (props) => {
   return (
-    <AppBar position="fixed" elevation={0} sx={{bgcolor: 'transparent'}}>
-      <Container maxWidth="xl" sx={{display: 'flex', py: 2}}>
+    <AppBar position="fixed" elevation={0} sx={{bgcolor: props.color || 'transparent'}}>
+      <Container maxWidth="xl" sx={{
+        display: 'flex',
+        py: 2,
+        px: { xs: 2, md: 10 },
+      }}>
         <Typography
           variant="h5"
           component={RLink}
           to="/"
-          sx={{ flexGrow: 1, fontWeight: 'medium', textDecoration: 'none', color: 'primary.main' }}>
+          sx={{ flexGrow: 1, fontWeight: 'medium', textDecoration: 'none', color: props.textColor || 'text.secondary' }}>
         Konsultant
         </Typography>
         <div>
-          <Typography
-            component={Link}
-            href="https://github.com/chiorji"
-            target="_blank"
-            rel="noopener noreferrer"
-            sx={{
-              mr: 5,
-              textDecoration: 'none',
-              color: 'text.primary',
-              fontWeight: 'medium',
-            }}
-        >Hire Me</Typography>
-        <IconButton sx={{color: 'primary.dark'}}>
-          <MenuIcon />
-        </IconButton>
+            <Typography
+              component={Link}
+              sx={{color: '#fff', mr: 3}}
+              href="https://github.com/chiorji/konsultant" target="_blank" rel="noopener noreferrer">
+              <GitHub fontSize="large"/>
+            </Typography>
+            <Typography
+              component={Link}
+              sx={{color: '#fff'}}
+              href="https://www.twitter.com/@ChigboguOrji" target="_blank" rel="noopener noreferrer">
+              <Twitter fontSize="large"/>
+            </Typography>
         </div>
       </Container>
     </AppBar>
